6cce55d927d3ba0a561cc2fe44d6c2a4c9b25345
Damni464 <damni5707@conestogac.on.ca>
2020-11-29 21:19:23 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_3_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
